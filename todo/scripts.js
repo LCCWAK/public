fd212c802220a534b82d086ba4259116cc2ad57a
Acrepi <86026769+Acrepi@users.noreply.github.com>
2022-10-22 22:08:44 +0200

    Add files via upload

--- todo/scripts.js
+++ todo/scripts.js
@@ -1,6 +1,88 @@
 //list of tasks IDs
 var items = [];
 
+//load tasks from localstorage
+function loadTasks(){
+  document.getElementById("addName").value = "";
+  document.getElementById("addDate").value = "";
+  document.getElementById("searchName").value = "";
+
+  var keys = Object.keys(localStorage);
+  var keysLength = keys.length;
+
+  if(keysLength > 0){
+    for(i=0; i<keysLength; i++){
+      if(keys[i].includes("Name")){
+        //console.log(keys[i]);
+        var item = keys[i].substr(4, keys[i].length - 8)
+        items.push(parseInt(item))
+      }
+    }
+    items.sort(function(a, b){return a-b});
+    //console.log(items);
+
+    for(i=0; i<items.length; i++){
+      var name = localStorage.getItem("task" + items[i] + "Name");
+      var date = localStorage.getItem("task" + items[i] + "Date");
+      createItem(items[i], name, date);
+    }
+  }
+}
+
+function createItem(itemNumber, name, date){
+  var row = document.createElement("div");
+	row.classList.add("row");
+	row.setAttribute("id", "item" + itemNumber);
+	document.getElementById("list").appendChild(row);
+
+  var rightBorder = document.createElement("div");
+	rightBorder.classList.add("rightBorder");
+	rightBorder.setAttribute("id", "item" + itemNumber + "a");
+	document.getElementById("item" + itemNumber).appendChild(rightBorder);
+
+  var rowName = document.createElement("div");
+  rowName.classList.add("rowName");
+  rowName.classList.add("rightBorder");
+  rowName.setAttribute("id", "item" + itemNumber + "Name");
+  rowName.setAttribute("onclick", "editTask(" +  itemNumber  + ")");
+  document.getElementById("item" + itemNumber + "a").appendChild(rowName);
+  document.getElementById("item" + itemNumber + "Name").innerHTML = name;
+  //document.getElementById("item" + itemNumber + "Name").style.display = "none";
+
+  var rowNameEditHolder = document.createElement("div");
+  rowNameEditHolder.classList.add("rowNameEditHolder");
+	rowNameEditHolder.classList.add("rightBorder");
+	rowNameEditHolder.setAttribute("id", "item" + itemNumber + "b");
+	document.getElementById("item" + itemNumber + "a").appendChild(rowNameEditHolder);
+  document.getElementById("item" + itemNumber + "b").style.display = "none";
+
+  var rowNameEdit = document.createElement("input");
+  rowNameEdit.classList.add("rowNameEdit");
+  rowNameEdit.setAttribute("id", "item" + itemNumber + "NameEdit");
+	rowNameEdit.setAttribute("type", "text");
+  rowNameEdit.setAttribute("onfocusout", "saveTask(" +  itemNumber  + ")");
+	document.getElementById("item" + itemNumber + "b").appendChild(rowNameEdit);
+  document.getElementById("item" + itemNumber + "NameEdit").value = name;
+
+  var rowDate = document.createElement("div");
+  rowDate.classList.add("rowDate");
+  rowDate.setAttribute("id", "item" + itemNumber + "Date");
+	document.getElementById("item" + itemNumber + "a").appendChild(rowDate);
+  document.getElementById("item" + itemNumber + "Date").innerHTML = date;
+
+  var rowButtonHolder = document.createElement("div");
+  rowButtonHolder.classList.add("rowButtonHolder");
+  rowButtonHolder.setAttribute("id", "item" + itemNumber + "ButtonHolder");
+	document.getElementById("item" + itemNumber).appendChild(rowButtonHolder);
+
+  var rowButton = document.createElement("a");
+  rowButton.classList.add("rowButton");
+  rowButton.setAttribute("id", "item" + itemNumber + "Button");
+  rowButton.setAttribute("onclick", "deleteTask(" +  itemNumber  + ")");
+	document.getElementById("item" + itemNumber + "ButtonHolder").appendChild(rowButton);
+  document.getElementById("item" + itemNumber + "Button").innerHTML = "DELETE";
+}
+
 //add task
 function addTask(){
   var name = document.getElementById("addName").value;
@@ -57,58 +139,10 @@ function addTask(){
   }
 
   //create html elements
-  var row = document.createElement("div");
-	row.classList.add("row");
-	row.setAttribute("id", "item" + itemNumber);
-	document.getElementById("list").appendChild(row);
+  createItem(itemNumber, name, date);
 
-  var rightBorder = document.createElement("div");
-	rightBorder.classList.add("rightBorder");
-	rightBorder.setAttribute("id", "item" + itemNumber + "a");
-	document.getElementById("item" + itemNumber).appendChild(rightBorder);
-
-  var rowName = document.createElement("div");
-  rowName.classList.add("rowName");
-  rowName.classList.add("rightBorder");
-  rowName.setAttribute("id", "item" + itemNumber + "Name");
-  rowName.setAttribute("onclick", "editTask(" +  itemNumber  + ")");
-  document.getElementById("item" + itemNumber + "a").appendChild(rowName);
-  document.getElementById("item" + itemNumber + "Name").innerHTML = name;
-  //document.getElementById("item" + itemNumber + "Name").style.display = "none";
-
-  var rowNameEditHolder = document.createElement("div");
-  rowNameEditHolder.classList.add("rowNameEditHolder");
-	rowNameEditHolder.classList.add("rightBorder");
-	rowNameEditHolder.setAttribute("id", "item" + itemNumber + "b");
-	document.getElementById("item" + itemNumber + "a").appendChild(rowNameEditHolder);
-  document.getElementById("item" + itemNumber + "b").style.display = "none";
-
-  var rowNameEdit = document.createElement("input");
-  rowNameEdit.classList.add("rowNameEdit");
-  rowNameEdit.setAttribute("id", "item" + itemNumber + "NameEdit");
-	rowNameEdit.setAttribute("type", "text");
-  rowNameEdit.setAttribute("onfocusout", "saveTask(" +  itemNumber  + ")");
-	document.getElementById("item" + itemNumber + "b").appendChild(rowNameEdit);
-  document.getElementById("item" + itemNumber + "NameEdit").value = name;
-
-  var rowDate = document.createElement("div");
-  rowDate.classList.add("rowDate");
-  rowDate.setAttribute("id", "item" + itemNumber + "Date");
-	document.getElementById("item" + itemNumber + "a").appendChild(rowDate);
-  document.getElementById("item" + itemNumber + "Date").innerHTML = date;
-
-  var rowButtonHolder = document.createElement("div");
-  rowButtonHolder.classList.add("rowButtonHolder");
-  rowButtonHolder.setAttribute("id", "item" + itemNumber + "ButtonHolder");
-	document.getElementById("item" + itemNumber).appendChild(rowButtonHolder);
-
-  var rowButton = document.createElement("a");
-  rowButton.classList.add("rowButton");
-  rowButton.setAttribute("id", "item" + itemNumber + "Button");
-  rowButton.setAttribute("onclick", "deleteTask(" +  itemNumber  + ")");
-	document.getElementById("item" + itemNumber + "ButtonHolder").appendChild(rowButton);
-  document.getElementById("item" + itemNumber + "Button").innerHTML = "DELETE";
-
+  window.localStorage.setItem("task" + itemNumber + "Name", name);
+  window.localStorage.setItem("task" + itemNumber + "Date", date);
   items.push(itemNumber);
 
   searchTasks();
@@ -151,6 +185,8 @@ function saveTask(task){
   document.getElementById("item" + task + "b").style.display = "none";
   document.getElementById("item" + task + "Name").innerHTML = name;
 
+  window.localStorage.setItem("task" + task + "Name", name);
+
   searchTasks();
 }
 
@@ -158,6 +194,9 @@ function saveTask(task){
 function deleteTask(task){
   //console.log(task)
   document.getElementById("item" + task).remove();
+
+  window.localStorage.removeItem("task" + task + "Name");
+  window.localStorage.removeItem("task" + task + "Date");
   var index = items.indexOf(task);
   items.splice(index, 1);
 }
